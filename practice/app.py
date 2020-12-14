2602be8076cbec515bd522f316eb22586129b0ab
Motoki <report.japan.hiroshima@gmail.com>
2020-12-14 23:32:48 +0900

    first commit

--- practice/app.py
+++ practice/app.py
@@ -6,34 +6,42 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     """初期画面を表示します."""
     return render_template("index.html")
 
+
 @app.route("/api/recommend_article")
 def api_recommend_article():
-    """はてブのホットエントリーから記事を入手して、ランダムに1件返却します."""
 
-    """
-        **** ここを実装します（基礎課題） ****
+    # 1. はてブのホットエントリーページのHTMLを取得する
+    url = "http://feeds.feedburner.com/hatena/b/hotentry"
+    with urlopen(url) as res:
+        html = res.read().decode('utf-8')
 
-        1. はてブのホットエントリーページのHTMLを取得する
-        2. BeautifulSoupでHTMLを読み込む
-        3. 記事一覧を取得する
-        4. ランダムに1件取得する
-        5. 以下の形式で返却する.
-            {
-                "content" : "記事のタイトル",
-                "link" : "記事のURL"
-            }
-    """
+    # 2. BeautifulSoupでHTMLを読み込む
+    soup = BeautifulSoup(html, "html.parser")
 
-    # ダミー
+    # 3. 記事一覧を取得する
+    items = soup.select("item")
+
+    # 4. ランダムに1件取得する
+    shuffle(items)
+    item = items[0]
+    print(item)
+
+    # 5. 以下の形式で返却する.
+    # {
+    #     "content" : "記事のタイトル",
+    #     "link" : "記事のURL"
+    # }
     return json.dumps({
-        "content" : "記事のタイトルだよー",
-        "link" : "記事のURLだよー"
+        "content": item.find("title").string,
+        "link": item.get("rdf:about")
     })
+
 
 @app.route("/api/xxxx")
 def api_xxxx():
@@ -44,6 +52,7 @@ def api_xxxx():
         ・関数名は適宜変更してください
     """
     pass
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5004)
